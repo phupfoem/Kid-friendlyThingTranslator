8df9139c2d025c7e7fcbbec293151576c2581dd7
LAPTOP-8HJKCFR7\Win 10 <chauandvi4@gmail.com>
2021-05-23 21:07:13 +0700

    Set up button in Home screen

--- App/src/main/java/com/example/main/MainActivity.java
+++ App/src/main/java/com/example/main/MainActivity.java
@@ -1,14 +1,33 @@
 package com.example.main;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button btnFav, btnRec, btnHis, btnCam;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //Favorite List
+        btnFav = (Button) findViewById(R.id.favoriteslist);
+        btnFav.setText("Favorites List");
+
+        //Rec words
+        btnRec = (Button) findViewById(R.id.recwords);
+        btnRec.setText("Words of the week");
+
+        //History search
+        btnHis = (Button) findViewById(R.id.historysearch);
+        btnHis.setText("History Search");
+
+        //Camera
+        btnCam = (Button) findViewById(R.id.camera);
+        btnCam.setText("Take a picture");
+
+
+
     }
 }
