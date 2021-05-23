dc58f4b1f45e772c41aad604e9f8ed21dbdb51de
LAPTOP-8HJKCFR7\Win 10 <chauandvi4@gmail.com>
2021-05-23 21:33:38 +0700

    Complete Home UI

--- App/src/main/java/com/example/main/MainActivity.java
+++ App/src/main/java/com/example/main/MainActivity.java
@@ -10,7 +10,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.homelayout);
         //Favorite List
         btnFav = (Button) findViewById(R.id.favoriteslist);
         btnFav.setText("Favorites List");
