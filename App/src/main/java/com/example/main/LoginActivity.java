b85ef5e4da4cd19ad5069a9bab700a3ac7a3aa83
LAPTOP-8HJKCFR7\Win 10 <chauandvi4@gmail.com>
2021-05-31 03:55:51 +0700

    Improve login signup UI

--- App/src/main/java/com/example/main/LoginActivity.java
+++ App/src/main/java/com/example/main/LoginActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,7 +13,8 @@ import androidx.appcompat.app.AppCompatActivity;
 public class LoginActivity extends AppCompatActivity {
     private EditText Name;
     private EditText Password;
-    private Button Login, Signup;
+    private Button Login;
+    private TextView Signup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,8 +24,7 @@ public class LoginActivity extends AppCompatActivity {
         Login =(Button) findViewById(R.id.login);
         Login.setText("LOG IN");
 
-        Signup = (Button) findViewById(R.id.signup);
-        Signup.setText("SIGN UP");
+        Signup = (TextView) findViewById(R.id.signup);
 
         Name = (EditText) findViewById(R.id.username);
         Password = (EditText) findViewById(R.id.password);
