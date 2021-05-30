7670331a37e98090422831c155f8a8a949970309
LAPTOP-8HJKCFR7\Win 10 <chauandvi4@gmail.com>
2021-05-30 23:37:19 +0700

    Basic Login Signup Frontend

--- App/src/main/java/com/example/main/SignupActivity.java
+++ App/src/main/java/com/example/main/SignupActivity.java
@@ -1,14 +1,34 @@
 package com.example.main;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class SignupActivity extends AppCompatActivity {
 
+    EditText Name, Email, Password, Confirm;
+    Button btnSignUp;
+    TextView textViewLogin;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+
+
+        //Go back to Log in Page
+        textViewLogin = (TextView)findViewById(R.id.login);
+        textViewLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SignupActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
