60dc95307d604a3cd6a769d3142a707bf1c97b35
LAPTOP-8HJKCFR7\Win 10 <chauandvi4@gmail.com>
2021-05-31 03:55:51 +0700

    Improve login signup UI

--- App/src/main/java/com/example/main/SignupActivity.java
+++ App/src/main/java/com/example/main/SignupActivity.java
@@ -19,9 +19,16 @@ public class SignupActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
 
+        btnSignUp = (Button) findViewById(R.id.btnSignUp);
+        textViewLogin = (TextView)findViewById(R.id.login);
+        Name = (EditText)findViewById(R.id.textNameSignUp);
+        Password = (EditText)findViewById(R.id.textPasswordSignUp);
+        Email = (EditText)findViewById(R.id.textEmailSignUp);
+        Confirm = (EditText)findViewById(R.id.textPasswordRetypeSignUp);
+
 
         //Go back to Log in Page
-        textViewLogin = (TextView)findViewById(R.id.login);
+
         textViewLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -29,6 +36,47 @@ public class SignupActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SignupActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
 
     }
+//    private Boolean validateName(){
+//        String val = Name.getText().toString();
+//        if (val.isEmpty()){
+//            Name.setError("Field cannot be empty");
+//            return false;
+//
+//        } else{
+//            Name.setError(null);
+//            return true;
+//        }
+//    }
+//    private Boolean validateEmail(){
+//        String val = Email.getText().toString();
+//        if (val.isEmpty()){
+//            Email.setError("Field cannot be empty");
+//            return false;
+//        }else{
+//            Email.setError(null);
+//            return true;
+//        }
+//    }
+//    private Boolean validatePassword(){
+//        String val = Password.getText().toString();
+//        if (val.isEmpty()){
+//            Password.setError("Field cannot be empty");
+//            return false;
+//
+//        }else{
+//            Password.setError(null);
+//            return true;
+//        }
+//    }
+
 }
