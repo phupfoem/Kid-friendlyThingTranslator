ae1278fef9da3141cc2b47e72ca8acd60793c68c
dunglam2000vn <83953749+dunglam2000vn@users.noreply.github.com>
2021-05-31 07:56:15 +0700

    add History Layout

--- App/src/main/java/com/example/main/HistoryActivity.java
+++ App/src/main/java/com/example/main/HistoryActivity.java
@@ -1,12 +1,8 @@
 package com.example.main;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
 import android.widget.ListView;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -14,7 +10,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class HistoryActivity extends AppCompatActivity {
     ListView simpleList; //change to Recycler View since its legacy now :(
-    String[] countryList = {"India", "China", "australia", "Portugle", "America", "NewZealand","1","2","3","4","5","6","7"};
+    String[] countryList = {"India", "China", "australia", "Portugle", "America", "NewZealand","1","2","3","4","5","6","7","8","9"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
